ba4ecb7fc7eca8c4641248fe4fde19759826d68a
Tim Graupmann <tgraupmann@gmail.com>
2021-09-26 16:44:23 -0700

    Added widget logic

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using Microsoft.Gaming.XboxGameBar;
+using System;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace Uwp_Crosshair
@@ -22,6 +15,8 @@ namespace Uwp_Crosshair
     /// </summary>
     sealed partial class App : Application
     {
+        private XboxGameBarWidget _mXboxGameBarWidget = null;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -37,8 +32,97 @@ namespace Uwp_Crosshair
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            XboxGameBarWidgetActivatedEventArgs widgetArgs = null;
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                var protocolArgs = args as IProtocolActivatedEventArgs;
+                string scheme = protocolArgs.Uri.Scheme;
+                if (scheme.StartsWith("ms-gamebarwidget"))
+                {
+                    widgetArgs = args as XboxGameBarWidgetActivatedEventArgs;
+                }
+            }
+
+            if (widgetArgs != null)
+            {
+                //
+                // Activation Notes:
+                //
+                //    If IsLaunchActivation is true, this is Game Bar launching a new instance
+                // of our widget. This means we have a NEW CoreWindow with corresponding UI
+                // dispatcher, and we MUST create and hold onto a new XboxGameBarWidget.
+                //
+                // Otherwise this is a subsequent activation coming from Game Bar. We MUST
+                // continue to hold the XboxGameBarWidget created during initial activation
+                // and ignore this repeat activation, or just observe the URI command here and act 
+                // accordingly.  It is ok to perform a navigate on the root frame to switch 
+                // views/pages if needed.  Game Bar lets us control the URI for sending widget to
+                // widget commands or receiving a command from another non-widget process. 
+                //
+                // Important Cleanup Notes:
+                //    When our widget is closed--by Game Bar or us calling XboxGameBarWidget.Close()-,
+                // the CoreWindow will get a closed event.  We can register for Window.Closed
+                // event to know when our partucular widget has shutdown, and cleanup accordingly.
+                //
+                // NOTE: If a widget's CoreWindow is the LAST CoreWindow being closed for the process
+                // then we won't get the Window.Closed event.  However, we will get the OnSuspending
+                // call and can use that for cleanup.
+                //
+                if (widgetArgs.IsLaunchActivation)
+                {
+                    Frame rootFrame = new Frame();
+                    rootFrame.NavigationFailed += OnNavigationFailed;
+                    Window.Current.Content = rootFrame;
+
+                    // Navigate to correct view
+                    switch (widgetArgs.AppExtensionId)
+                    {
+                        case "ExtensionCrosshair":
+                            _mXboxGameBarWidget = new XboxGameBarWidget(
+                                widgetArgs,
+                                Window.Current.CoreWindow,
+                                rootFrame);
+
+                            rootFrame.Navigate(typeof(MainPage), _mXboxGameBarWidget);
+                            break;                        
+                        default:
+                            // Unknown - Game Bar should never send you an unknown App Extension Id
+                            return;
+                    }
+
+                    Window.Current.Activate();
+                }
+            }
+        }
+
+        public void ShowMainPage()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(MainPage), _mXboxGameBarWidget);
+            rootFrame.Width = 500;
+            rootFrame.Height = 500;
+            Size size = new Size(rootFrame.Width, rootFrame.Height);
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(size);
+            ApplicationView.GetForCurrentView().TryResizeView(size);
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
+
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = false;
+            }
+#endif
+
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
@@ -47,6 +131,8 @@ namespace Uwp_Crosshair
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
+                // Set the default language
+                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
@@ -59,18 +145,17 @@ namespace Uwp_Crosshair
                 Window.Current.Content = rootFrame;
             }
 
-            if (e.PrelaunchActivated == false)
+            if (rootFrame.Content == null)
             {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+
+                ShowMainPage();
             }
+
+            // Ensure the current window is active
+            Window.Current.Activate();
         }
 
         /// <summary>
